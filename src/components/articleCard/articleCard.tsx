abad44fc6eba40c60c72d9a900aacd7591201e8e
Sid <a15348545942@outlook.com>
2020-10-03 21:11:28 +0800

    design the article card component

---
+++ src/components/articleCard/articleCard.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react'
+import './articleCard.css'
+
+export interface Props {
+  title: string
+  datetime: string
+  category: string
+  tags: string[]
+  part: string
+}
+
+export default function ArticleCard({
+  title,
+  datetime,
+  category,
+  tags,
+  part,
+}: Props) {
+  return (
+    <div className="articleCard">
+      <div className="article-title">{title}</div>
+      <div className="article-infor">
+        <div className="first-infor">
+          <div className="article-datetime">
+            <i className="article-icon fa fa-clock" /> {datetime}
+          </div>
+          <span />
+          <div className="article-category">
+            <i className="article-icon fa fa-cube" />
+            <a href={'#/category/' + category} className="article-block">
+              <span className="block">{category}</span>
+            </a>
+          </div>
+        </div>
+        <div className="article-tags">
+          <i className="article-icon fa fa-tag" />
+          <div className="tagList">
+            {tags.map(tag => {
+              return (
+                <a href={'#/tag/' + tag} className="article-block">
+                  <span className="block">{tag}</span>
+                </a>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="article-part">{part}</div>
+				<a className="readMore" href={"#/article/"+{title}}>
+        Read More <span>>></span>
+      </a>
+      <span className="separat" />
+    </div>
+  )
+}
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,14 +1,28 @@
 import * as React from 'react'
 import MainNav from '../mainNav/mainNav'
+import ArticleCard from '../articleCard/articleCard'
 import './home.css'
 
-export default class Home extends React.Component{
-	render(){
-		return(
-			<div className="home">
-				<div className="title"><i className="icon fa fa-rocket"/>Sid's Blog</div>
-				<MainNav />
-			</div>
-		)
-	}
+export default class Home extends React.Component {
+  testArticle = {
+    title: '测试文章',
+    datetime: '2020-10-03',
+    category: 'test',
+    tags: ['test', 'tagTest', 'typescript', 'blog'],
+    part:
+      '这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章。\n这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章。\n这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章，这是用来测试的文章。\n',
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <div className="title">
+          <i className="icon fa fa-rocket" />
+          Sid's Blog
+        </div>
+        <MainNav />
+        <ArticleCard {...this.testArticle} />
+      </div>
+    )
+  }
 }
